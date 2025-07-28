2298e0938dd1321c0290448916e5091da5d89651
hari-1222 <hariprasath3241@gmail.com>
2025-07-28 23:27:11 +0530

    Add files via upload

---
+++ math operations.py
@@ -0,0 +1,10 @@
+a=int(input('enter the first number:'))
+b=int(input('enter the second number:'))
+c=a+b
+e=a-b
+f=a*b
+g=a/b
+print('addition:',c)
+print('subtraction:',e)
+print('multiplication',f)
+print('division:',g)
---
+++ personalized greeting.py
@@ -0,0 +1,5 @@
+a=str(input('enter a first name:'))
+b=str(input('enter a last name:'))
+c=a+b
+print(c)
+print(f"hello ,{c}! welcome to the python program")
